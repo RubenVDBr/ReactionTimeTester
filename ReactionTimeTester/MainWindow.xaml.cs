e3795979a92effa505c2b0d8659bf3451b66dbaf
RubenVDBr <47314701+RubenVDBr@users.noreply.github.com>
2021-05-29 16:39:34 +0200

    Add UI to enter highscore username

--- ReactionTimeTester/MainWindow.xaml.cs
+++ ReactionTimeTester/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace ReactionTimeTester
         {
             InitializeComponent();
         }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
