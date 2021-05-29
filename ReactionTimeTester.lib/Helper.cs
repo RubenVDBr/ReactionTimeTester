972cbe0cff2a3b6e2a0eef45c879d02f67a5ba81
Lhamo <lhamo.denys@student.howest.be>
2021-05-29 16:55:56 +0200

    Add Method in HelperClass + entities

---
+++ ReactionTimeTester.lib/Helper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ReactionTimeTester.lib
+{
+    public class Helper
+    {
+        public List<string> GetActiveIP4s()
+        {
+            List<string> activeIps = new List<string>();
+            activeIps.Add("127.0.0.1");
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            
+            foreach(var ip in host.AddressList)
+            {
+                if(ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    activeIps.Add(ip.ToString());
+                }
+            }
+            return activeIps;
+        }
+    }
+}
